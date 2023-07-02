c179e65b66b1297418123211301c0ab8a05cf677
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-02 22:36:18 +0100

    Navbar Component - Done

--- hookbank_project/src/App.jsx
+++ hookbank_project/src/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./style";
 import {
+	Billing,
 	Navbar,
 	Hero,
 	Stats,
@@ -13,7 +14,7 @@ import {
 
 const App = () => {
 	return (
-		<div className='bg-primary w-full overdlow-hidden'>
+		<div className='bg-primary w-full overflow-hidden'>
 			<div className={`${styles.paddingX} ${styles.flexCenter}`}>
 				<div className={`${styles.boxWidth}`}>
 					<Navbar />
@@ -27,7 +28,8 @@ const App = () => {
 
 			<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
 				<div className={`${styles.boxWidth}`}>
-					<Stats /> <Business /> <CardDeal /> <Testimonials /> <Clients />
+					<Stats /> <Business /> <Billing />
+					<CardDeal /> <Testimonials /> <Clients />
 					<CTA /> <Footer />
 				</div>
 			</div>
--- hookbank_project/src/components/Navbar.jsx
+++ hookbank_project/src/components/Navbar.jsx
@@ -37,12 +37,12 @@ const Navbar = () => {
 						toggle ? "flex" : "hidden"
 					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
 				>
-					<ul className='list-none flex justify-end items-center flex-1'>
+					<ul className='list-none flex flex-col justify-end items-center flex-1'>
 						{navLinks.map((nav, index) => (
 							<li
 								key={nav.id}
 								className={`font-poppins font-normal cursor-pointer text-[16px] ${
-									index === navLinks.length - 1 ? "mr-0" : "mr-10"
+									index === navLinks.length - 1 ? "mb-0" : "mb-4"
 								} text-white`}
 							>
 								<a href={`#${nav.id}`}>{nav.title}</a>
