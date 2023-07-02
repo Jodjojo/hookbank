ce55abf47cebd7fb8d20979cebfa42c215f7e8c0
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-02 20:15:01 +0100

    Final Basic Structure for webpage

--- hookbank_project/src/App.jsx
+++ hookbank_project/src/App.jsx
@@ -1,18 +1,34 @@
 import styles from "./style";
+import {
+	Navbar,
+	Hero,
+	Stats,
+	Business,
+	CardDeal,
+	Testimonials,
+	Clients,
+	CTA,
+	Footer,
+} from "./components/index";
 
 const App = () => {
 	return (
 		<div className='bg-primary w-full overdlow-hidden'>
 			<div className={`${styles.paddingX} ${styles.flexCenter}`}>
-				<div className={`${styles.boxWidth}`}>Navbar</div>
+				<div className={`${styles.boxWidth}`}>
+					<Navbar />
+				</div>
 			</div>
 			<div className={`bg-primary ${styles.flexStart}`}>
-				<div className={`${styles.boxWidth}`}>Hero</div>
+				<div className={`${styles.boxWidth}`}>
+					<Hero />
+				</div>
 			</div>
 
 			<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
 				<div className={`${styles.boxWidth}`}>
-					Stats Business CardDeal Testimonials Clients CTA Footer
+					<Stats /> <Business /> <CardDeal /> <Testimonials /> <Clients />
+					<CTA /> <Footer />
 				</div>
 			</div>
 		</div>
