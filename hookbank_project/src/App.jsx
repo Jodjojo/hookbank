4862f8a40baa296d8f9ef382576d25b8e7ab712c
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-02 20:07:42 +0100

    Component Organization and file creation

--- hookbank_project/src/App.jsx
+++ hookbank_project/src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import styles from "./style";
 
 const App = () => {
-	return <div className='bg-primary w-full overdlow-hidden'></div>;
+	return (
+		<div className='bg-primary w-full overdlow-hidden'>
+			<div className={`${styles.paddingX} ${styles.flexCenter}`}>
+				<div className={`${styles.boxWidth}`}>Navbar</div>
+			</div>
+			<div className={`bg-primary ${styles.flexStart}`}>
+				<div className={`${styles.boxWidth}`}>Hero</div>
+			</div>
+
+			<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+				<div className={`${styles.boxWidth}`}>
+					Stats Business CardDeal Testimonials Clients CTA Footer
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
