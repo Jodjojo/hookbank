e2c163ec8974bac213bebee3fd31758a877c8c07
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-02 22:24:03 +0100

    Navbar Component - Tailwind 1

--- hookbank_project/src/components/Navbar.jsx
+++ hookbank_project/src/components/Navbar.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const [toggle, settoggle] = useState(false);
+
+	return (
+		<nav className='w-full flex py-6 justify-between items-center navbar'>
+			<img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+			<ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+				{navLinks.map((nav, index) => (
+					<li
+						key={nav.id}
+						// classnames meanings can be gotten from the tailwind documentation
+						// the navlink operator is used to check if the last link the margin should be 0
+						className={`font-poppins font-normal cursor-pointer text-[16px] ${
+							index === navLinks.length - 1 ? "mr-0" : "mr-10"
+						} text-white`}
+					>
+						<a href={`#${nav.id}`}>{nav.title}</a>
+					</li>
+				))}
+			</ul>
+
+			<div className='sm:hidden flex flex-1 justify-end items-center'>
+				<img
+					src={toggle ? close : menu}
+					alt='menu'
+					className='w-[28px] h-[28px] object-contain'
+					// chnaging the state of the useState without setting it to the previous version of the state
+					onClick={() => settoggle((prev) => !prev)}
+				/>
+				{/* The div ternary operator to set if the toggle is turned display is set to flex, otherwise hidden  */}
+				<div
+					className={`${
+						toggle ? "flex" : "hidden"
+					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+				>
+					<ul className='list-none flex justify-end items-center flex-1'>
+						{navLinks.map((nav, index) => (
+							<li
+								key={nav.id}
+								className={`font-poppins font-normal cursor-pointer text-[16px] ${
+									index === navLinks.length - 1 ? "mr-0" : "mr-10"
+								} text-white`}
+							>
+								<a href={`#${nav.id}`}>{nav.title}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
