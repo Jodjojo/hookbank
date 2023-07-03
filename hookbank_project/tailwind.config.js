fbb9a739c2e32f3552cd7e9f2dc193238bc75c44
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-03 16:33:42 +0100

    Hero Component  - Tailwind

--- hookbank_project/tailwind.config.js
+++ hookbank_project/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
 			colors: {
 				primary: "#00040f",
 				secondary: "#00f6ff",
+				tertiary: "#113a3b ",
 				dimWhite: "rgba(255, 255, 255, 0.7)",
 				dimBlue: "rgba(9, 151, 124, 0.1)",
 			},
